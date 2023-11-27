53e0e526c0cf327f1fe771f657ed90f4e22ed090
Ezekiel200483 <olopade.ezekiel1998@gmail.com>
2023-11-27 12:28:01 +0100

    New Assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import MovieList from './component/MovieList';
+import Filter from './component/Filter';
+import movies from './component/moviesData'; // Import the movies array
+const App = () => {
+  const [moviesData, setMoviesData] = useState(movies);
 
-function App() {
+  const [filteredMovies, setFilteredMovies] = useState(moviesData);
+
+  const handleFilterChange = (filters) => {
+    // Implement filtering logic based on title and rate filters
+    const filtered = movies.filter((movie) => {
+      const titleMatch = movie.title
+        .toLowerCase()
+        .includes(filters.title.toLowerCase());
+      const rateMatch =
+        filters.rating === "" || movie.rating >= parseInt(filters.rating);
+
+      return titleMatch && rateMatch;
+    });
+
+    setFilteredMovies(filtered);
+  };
+
+  const handleAddMovie = (newMovie) => {
+    setMoviesData((prevMovies) => [...prevMovies, { id: Date.now(), ...newMovie }]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>NETFLIX</h1>
+      <Filter onFilterChange={handleFilterChange} />
+      <MovieList movies={filteredMovies} />
+      <AddMovieForm onAddMovie={handleAddMovie} />
     </div>
   );
-}
+};
+
+const AddMovieForm = ({ onAddMovie }) => {
+  const [newMovie, setNewMovie] = useState({ title: '', description: '', rating: '', posterURL: '' });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewMovie((prevMovie) => ({ ...prevMovie, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Validate the new movie data 
+    if (newMovie.title && newMovie.rating && newMovie.posterURL) {
+      onAddMovie(newMovie);
+      setNewMovie({ title: '', description: '', rating: '', posterURL: '' });
+    }
+  };
+
+  return (
+    <form className="add-movie-form" onSubmit={handleSubmit}>
+      <h2>Add a New Movie</h2>
+      <label>
+        Title:
+        <input type="text" name="title" value={newMovie.title} onChange={handleChange} />
+      </label>
+      <label>
+        Description:
+        <textarea name="description" value={newMovie.description} onChange={handleChange} />
+      </label>
+      <label>
+        Rating:
+        <input type="number" name="rating" value={newMovie.rating} onChange={handleChange} />
+      </label>
+      <label>
+        Poster URL:
+        <input type="text" name="posterURL" value={newMovie.posterURL} onChange={handleChange} />
+      </label>
+      <button type="submit">Add Movie</button>
+    </form>
+  );
+};
 
 export default App;
+
+
+
+
---
+++ src/component/moviesData.jsx
@@ -0,0 +1,78 @@
+import House from '../images/House of cards.jpeg'
+import Ozark from "../images/ozark.jpeg";
+import Sense from "../images/sense.jpeg";
+import Alien from "../images/Alien.jpeg";
+import Murder from "../images/Murder.jpeg";
+import Sheldon from "../images/Sheldon.jpeg";
+import Peaky from "../images/Peaky.jpeg";
+import Code from "../images/code.jpeg";
+
+
+const movies = [
+  {
+    id: 1,
+    title: "HOUSE OF CARDS",
+    description:
+      "Betrayed by the White House, Congressman Frank Underwood embarks on a ruthless rise to power. Blackmail, seduction and ambition are his weapons.",
+    rating: 4,
+    posterURL: House,
+  },
+  {
+    id: 2,
+    title: "OZARK",
+    description:
+      "A financial adviser drags his family from Chicago to the Missouri Ozarks, where he must launder $500 million in five years to appease a drug boss.",
+    rating: 3,
+    posterURL: Ozark,
+  },
+  {
+    id: 3,
+    title: "Sense8",
+    description:
+      "Eight strangers from around the globe suddenly begin to communicate telepathically, but soon discover their gift also makes them targets.",
+    rating: 3,
+    posterURL: Sense,
+  },
+  {
+    id: 4,
+    title: "The Alienist",
+    description:
+      "In 1896 New York City, psychologist Laszlo Kreizler is enlisted by Police Commissioner Theodore Roosevelt to catch a killer preying on homeless boys. ",
+    rating: 3,
+    posterURL: Alien,
+  },
+  {
+    id: 5,
+    title: "How to Get away with Murderer ",
+    description:
+      "Brilliant criminal defense attorney and law professor Annalise Keating, plus five of her students, become involved in a twisted murder case.",
+    rating: 3,
+    posterURL: Murder,
+  },
+  {
+    id: 6,
+    title: "Young Sheldon",
+    description:
+      "Meet a child genius named Sheldon Cooper (already seen as an adult in The Big Bang Theory (2007)) and his family",
+    rating: 3,
+    posterURL: Sheldon,
+  },
+  {
+    id: 7,
+    title: "Peaky Blinders",
+    description:
+      " Peaky Blinders is a crime drama centred on a family of mixed Irish Traveller and Romani origins based in Birmingham, England, starting in 1919",
+    rating: 5,
+    posterURL: Peaky,
+  },
+  {
+    id: 8,
+    title: "Criminal Code",
+    description:
+      "Criminal Code is a Portuguese crime thriller on Netflix that blends reality with fiction, exploring organized crime in Brazil",
+    rating: 3,
+    posterURL: Code,
+  },
+];
+
+export default movies;
